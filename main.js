147ec5f588c35b942f32a0282c7b10267b84a66d
Skun01 <truongg9655@gmail.com>
2024-05-26 21:20:05 +0700

    finish the UI

---
+++ main.js
@@ -0,0 +1,74 @@
+// main algorithm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const createKeyTool = document.querySelector('.create-key-section .btn');
+const createKeySection = document.querySelector('.form-1');
+
+const createSignatureTool = document.querySelector('.create-signature .btn');
+const createSignatureSection = document.querySelector('.signature');
+
+const verifyingTool = document.querySelector('.verifying-signature .btn')
+const verifyingSection = document.querySelector('.verifying');
+
+createKeyTool.addEventListener('click', e=>{
+    createKeyTool.classList.add('btn-active');
+    createSignatureTool.classList.remove('btn-active');
+    verifyingTool.classList.remove('btn-active');
+
+    createKeySection.style.display = "block";
+    createSignatureSection.style.display = 'none';
+    verifyingSection.style.display = 'none';
+});
+
+createSignatureTool.addEventListener('click', e=>{
+    createKeyTool.classList.remove('btn-active');
+    createSignatureTool.classList.add('btn-active');
+    verifyingTool.classList.remove('btn-active');
+
+    createKeySection.style.display = "none";
+    createSignatureSection.style.display = 'block';
+    verifyingSection.style.display = 'none';
+});
+
+verifyingTool.addEventListener('click', e=>{
+    createKeyTool.classList.remove('btn-active');
+    createSignatureTool.classList.remove('btn-active');
+    verifyingTool.classList.add('btn-active');
+
+    createKeySection.style.display = "none";
+    createSignatureSection.style.display = 'none';
+    verifyingSection.style.display = 'block';
+});
